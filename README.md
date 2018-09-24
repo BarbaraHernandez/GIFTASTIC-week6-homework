8baf7af97b7c8ec7c1008a24cf94f6d36533cb95
BarbaraHernandez <bandezanne@gmail.com>
2018-09-23 22:27:03 -0400

    added all files, basic HTML, and began javascript

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # GIFTASTIC-week6-homework
 Week 6 Homework - using GIPHY API to serve up dog GIFs
+
+About my App:
+
+About my Code: The most important part of this code is the array. We need to be able to populate buttons on the page using the contents of the array. We need to make sure that those buttons are functional in that they use GIPHY API to populate GIFs to the designated div. Finally, we need ensure that we can continue to add to the array from the app and have those new entries also populate functioning buttons. I think about in this order:
+    1. Create array - done, easy
+    2. Create buttons from array - done, easy
+    3. Populate GIFs using buttons
+    4. Append to the array from the DOM
+
+Our GIFs also need to load in a static state and play when clicked, going static again if clicked again.
+
+About my Resources:
+GIPHY API by giphy.com 
+getbootstrap.com
---
+++ assets/javascript/javascript.js
@@ -0,0 +1,53 @@
+//variables
+var dogBreed;
+var myDogs = ["pug", "corgi","shiba inu","pomeranian","french bulldog", "chihuahua", "dachshund"];
+
+for (i=0; i < myDogs.length; i++){
+    dogBreed = myDogs[i];
+    $("#button-bar").append("<button>" + dogBreed + "</button>");
+}
+
+$("button").on("click", function getGif(){
+    var dog = $(this).text;
+
+    //create link to query
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + dog + "&api_key=skroQi8RIIYW717TOet3Fnqg1j33oYuY&limit=10";
+
+    //define AJAX request -- I'm getting an error that this is not a function?
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+        //use data from request
+        .then(function(response){
+            console.log(queryURL);
+            console.log(response);
+            var results = response.data;
+
+            //loop through resulting GIFs, creating html elements
+            for (var i =0; i < results.length; i++){
+                var gifDiv = $("<div>");
+                var rating = $("<p>").text("Rating: " + results[i].rating);
+                var dogGif = $("<img>");
+                dogGif.attr("src", results[i].images.fixed_height.url);
+                gifDiv.append(rating);
+                gifDiv.append(dogGif);
+                $("#gif-box").prepend(gifDiv);//pushes populated gif div to DOM
+            }
+        });
+
+    
+});
+
+$(".gif").on("click", function() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+
