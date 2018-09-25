ace3ad038a7725f2f46a2cddc9680e674404f159
BarbaraHernandez <bandezanne@gmail.com>
2018-09-24 23:17:25 -0400

    set GIFs to load in still by default

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,14 +1,17 @@
 //variables
 var dogBreed;
-var myDogs = ["pug", "corgi","shiba inu","pomeranian","french bulldog", "chihuahua", "dachshund"];
+var topics = ["pug", "corgi","shiba inu","pomeranian","french bulldog", "chihuahua", "dachshund"];
 
-for (i=0; i < myDogs.length; i++){
-    dogBreed = myDogs[i];
+for (i=0; i < topics.length; i++){
+    dogBreed = topics[i];
     $("#button-bar").append("<button>" + dogBreed + "</button>");
 }
 
 $("button").on("click", function getGif(){
-    var dog = $(this).text;
+
+    $("#gif-box").empty();
+
+    var dog = $(this).text();
 
     //create link to query
     //something about this is not working. I am getting GIFs but dog is not working
@@ -31,7 +34,10 @@ $("button").on("click", function getGif(){
                 var gifDiv = $("<div>");
                 var rating = $("<p>").text("Rating: " + results[i].rating);
                 var dogGif = $("<img>");
-                dogGif.attr("src", results[i].images.fixed_height.url);
+                dogGif.attr("data-still", results[i].images.fixed_height_small_still.url);
+                dogGif.attr("data-animate", results[i].images.fixed_height_small.url);            
+                dogGif.attr("src", results[i].images.fixed_height_small_still.url);//load still gif by default
+                dogGif.attr("data-state", "still");
                 gifDiv.append(rating);
                 gifDiv.append(dogGif);
                 $("#gif-box").prepend(gifDiv);//pushes populated gif div to DOM
@@ -41,14 +47,21 @@ $("button").on("click", function getGif(){
     
 });
 
-$(".gif").on("click", function() {
+$(document).on("click", function() {
     var state = $(this).attr("data-state");
     if (state === "still") {
       $(this).attr("src", $(this).attr("data-animate"));
       $(this).attr("data-state", "animate");
+      console.log($(this).src);
     } else {
       $(this).attr("src", $(this).attr("data-still"));
       $(this).attr("data-state", "still");
     }
-  });
+});
+
+$("#submit").on("click", function (){
+    var newDog = $("#user-input").val().trim();
+    console.log(newDog);
+    topics.push(newDog);
+})
 
